33b83913dd60f40a570083dd12778e6fc1144fa7
yuuto.ueda <kakegawayuuri@gmail.com>
2024-03-31 17:32:08 +0900

    フラッシュメッセージを追加

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -49,6 +49,7 @@ public class CreateServlet extends HttpServlet {
 
             em.persist(t);
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "登録が完了しました。");
             em.close();
 
             response.sendRedirect(request.getContextPath() + "/index");
